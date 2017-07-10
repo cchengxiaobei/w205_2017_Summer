dea00a9e29589c623b1d0b18f7ddbfc27f320eaa
cchengxiaobei <cchengxiaobei@gmail.com>
2017-07-09 19:52:07 -0700

    Update best_hospital.sql

--- exercise_1/investigations/best_hospital.sql
+++ exercise_1/investigations/best_hospital.sql
@@ -1,1 +1,9 @@
-select hospital_name, max(score_adjust) as score_max, sum(score_adjust) as aggregate_score, avg(score_adjust) as avg_score, variance(score_adjust) as variability  from my_care group by hospital_name order by score_max desc limit 10;
+select hospital_name, 
+        max(score_adjust) as score_max, 
+        sum(score_adjust) as aggregate_score, 
+        avg(score_adjust) as avg_score, 
+        variance(score_adjust) as variability  
+from my_care 
+group by hospital_name 
+order by score_max 
+desc limit 10;
