0e584f3c89a77ec453304f5dc25c0437caa2d7c1
w205 <w205@ip-172-31-19-120.ec2.internal>
2017-07-10 02:39:18 +0000

    investigation on best hospital

---
+++ exercise_1/investigations/best_hospital.sql
@@ -0,0 +1,1 @@
+select hospital_name, max(score_adjust) as score_max, sum(score_adjust) as aggregate_score, avg(score_adjust) as avg_score, variance(score_adjust) as variability  from my_care group by hospital_name order by score_max desc limit 10;
