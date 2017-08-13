5e96f9680b3d4fbd87a3572213e6f00322fd84ad
w205 <w205@ip-172-31-34-134.ec2.internal>
2017-08-13 00:39:44 +0000

    count word and add word into tweetwordcount table

--- exercise_2/extweetwordcount/src/bolts/wordcount.py
+++ exercise_2/extweetwordcount/src/bolts/wordcount.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import Counter
 from streamparse.bolt import Bolt
-
+import psycopg2
 
 
 class WordCounter(Bolt):
@@ -18,11 +18,29 @@ class WordCounter(Bolt):
         # Database name: Tcount 
         # Table name: Tweetwordcount 
         # you need to create both the database and the table in advance.
-        
-
+        conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+#conn.close()        
+        #cur = conn.cursor()
         # Increment the local count
         self.counts[word] += 1
         self.emit([word, self.counts[word]])
+ #       conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
+        cur = conn.cursor()
+        
+       # if self.counts[word] > 1: 
+        cur.execute("UPDATE tweetwordcount SET count=count+1 WHERE word=%s", (word,))
+       # print "number of updated rows", cur.rowcount
+        # conn.commit()
 
+        if self.counts[word]==1:
+               cur.execute("INSERT INTO tweetwordcount (word,count) VALUES (%s, 1)",(word,))
+        #	self.log('new')
+        conn.commit() 
+         #      cur.execute("UPDATE tweetwordcount SET count=count+1 WHERE word=%s", (word,))
+       # print "number of updated rows", cur.rowcount
+
+        #self.emit([word, self.counts[word]])
         # Log the count - just to see the topology running
         self.log('%s: %d' % (word, self.counts[word]))
+  	
+	conn.close()
